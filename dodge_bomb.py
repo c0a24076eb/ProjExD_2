8dbbe240099ac8feb628b1b5480fc8117a5789d0
Kawahara <c0a24076eb@edu.teu.ac.jp>
2026-06-16 14:40:55 +0900

    演習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,24 @@ def gameover(screen: pg.Surface) -> None:
     pg.display.update()
     pg.time.wait(5000)
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+
+    #大きさの異なる爆弾Surfaceと加速度リストを作成する関数
+    # 引数：なし
+    # 戻り値：爆弾Surfaceのリスト，加速度のリスト
+    
+    bb_imgs = []
+    bb_accs = []
+
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+        bb_accs.append(r)
+
+    return bb_imgs, bb_accs
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -60,9 +78,8 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
-    bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
+    bb_imgs, bb_accs = init_bb_imgs()
+    bb_img = bb_imgs[0]
     bb_rct = bb_img.get_rect()
     bb_rct.center = (
         random.randint(0, WIDTH),
@@ -75,10 +92,25 @@ def main():
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
+
         screen.blit(bg_img, [0, 0])
-        bb_rct.move_ip(vx, vy)
+
+        # 爆弾の大きさと速度を時間経過で変更
+        idx = min(tmr // 500, 9)
+        bb_img = bb_imgs[idx]
+        avx = vx * bb_accs[idx]
+        avy = vy * bb_accs[idx]
+
+        # 爆弾画像サイズにRectを合わせる
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+
+        # 爆弾移動
+        bb_rct.move_ip(avx, avy)
+
+        # 壁反射
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
@@ -87,21 +119,25 @@ def main():
 
         screen.blit(bb_img, bb_rct)
 
+        # こうかとん移動
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
         for k, mv in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
 
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
+        # 衝突判定
         if kk_rct.colliderect(bb_rct):
             gameover(screen)
             return
-        
+
         screen.blit(kk_img, kk_rct)
 
         pg.display.update()
